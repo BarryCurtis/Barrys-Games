9dd058fbe99b89fe80a02896ef5969a6c27cfee9
BarryCurtis <barrycurtis@hotmail.com>
2022-07-04 15:00:07 +0100

    small edits

---
+++ __tests__/app.test.js
@@ -0,0 +1,47 @@
+const connection = require("../db/connection");
+const app = require("../app");
+const seed = require("../db/seeds/seed");
+const {
+  categoryData,
+  commentData,
+  reviewData,
+  userData,
+} = require("../db/data/test-data/");
+const request = require("supertest");
+
+beforeEach(() => seed({ categoryData, commentData, reviewData, userData }));
+afterAll(() => connection.end());
+
+describe("GET /api/categories", () => {
+  test("200 response returns with an array", () => {
+    return request(app)
+      .get("/api/categories")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body).toBeInstanceOf(Array);
+      });
+  });
+  test("200 response returns array of objects with specific properties", () => {
+    return request(app)
+      .get("/api/categories")
+      .expect(200)
+      .then(({ body }) => {
+        body.forEach((category) => {
+          expect(category).toEqual(
+            expect.objectContaining({
+              slug: expect.any(String),
+              description: expect.any(String),
+            })
+          );
+        });
+      });
+  });
+  test("404 response returns an error path not found", () => {
+    return request(app)
+      .get("/api/categor")
+      .expect(404)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Bad request, path not found");
+      });
+  });
+});
---
+++ controllers/categories-controllers.js
@@ -0,0 +1,13 @@
+const fetchCategories = require("../models/categories-models");
+
+function getCategories(req, res) {
+  return fetchCategories()
+    .then((category) => {
+      res.status(200).send({ category });
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+module.exports = getCategories;
