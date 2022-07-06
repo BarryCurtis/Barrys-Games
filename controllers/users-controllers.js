50d75130be5164b506cc72bae535791ca76e2f61
BarryCurtis <barrycurtis@hotmail.com>
2022-07-06 11:05:34 +0100

    made changes to name of users in users-controllers file

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -2,8 +2,8 @@ const { fetchUsers } = require("../models/users-models.js");
 
 exports.getUsers = (req, res, next) => {
   return fetchUsers()
-    .then((user) => {
-      res.status(200).send({ users: user });
+    .then((users) => {
+      res.status(200).send(users);
     })
     .catch((err) => {
       next(err);
