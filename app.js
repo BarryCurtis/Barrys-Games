ac7ce98e1ec3735679f7545d2416924f7ff6d9d9
BarryCurtis <barrycurtis@hotmail.com>
2022-07-25 10:47:28 +0100

    add cors to app.js

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ const {
   postReviewComment,
 } = require("./controllers/reviews-controllers");
 const { getUsers } = require("./controllers/users-controllers");
+const cors = require("cors");
+
+app.use(cors());
 
 app.use(express.json());
 
